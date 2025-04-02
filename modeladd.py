6cf94c24b36bb36fd220592fe0e4dbc7611ca5b1
Prasoon Bhatt <101729597+mlprasoon@users.noreply.github.com>
2025-04-02 23:44:43 +0530

    Update modeladd.py

--- modeladd.py
+++ modeladd.py
@@ -2,5 +2,5 @@ import requests
 
 url = "https://github.com/mlprasoon/BrainCheck/releases/download/model/best_model.keras"
 response = requests.get(url)
-with open("model.h5", "wb") as f:
+with open("best_model.keras", "wb") as f:
     f.write(response.content)
