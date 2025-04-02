8ec917080786bda4bf0e0aaa3197fe29f050bac8
Prasoon Bhatt <101729597+mlprasoon@users.noreply.github.com>
2025-04-02 23:48:45 +0530

    Delete modeladd.py

--- modeladd.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = "https://github.com/mlprasoon/BrainCheck/releases/download/model/best_model.keras"
-response = requests.get(url)
-with open("best_model.keras", "wb") as f:
-    f.write(response.content)
